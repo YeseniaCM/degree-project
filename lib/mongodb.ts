3c64c8d5572b90c7499e84d8951b77539e7bb6db
YeseniaCM <yeseniacamilla@hotmail.com>
2025-01-29 21:19:50 +0100

    -

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -9,8 +9,7 @@ let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+
 
   const globalWithMongoClientPromise = global as typeof globalThis & {
     _mongoClientPromise: Promise<MongoClient>;
@@ -23,11 +22,9 @@ if (process.env.NODE_ENV === "development") {
 
   clientPromise = globalWithMongoClientPromise._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable.
+
   client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
 export default clientPromise;
